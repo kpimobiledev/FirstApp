80bbb79ed84ba3751c5d58977497ba346e1d8e10
lidaamber <lidaamber@gmail.com>
2020-02-25 22:35:14 +0200

    create broadcasts example

--- app/src/main/java/com/lidaamber/myapplication/MainActivity.kt
+++ app/src/main/java/com/lidaamber/myapplication/MainActivity.kt
@@ -1,25 +1,67 @@
 package com.lidaamber.myapplication
 
+import android.bluetooth.BluetoothAdapter
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bluetoothReceiver: BroadcastReceiver
+    private lateinit var greetingReceiver: BroadcastReceiver
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         greetButton.setOnClickListener {
-            val name = nameEditText.text.toString()
+            val intent = Intent(GREET_ACTION)
+            intent.putExtra(EXTRA_NAME, nameEditText.text.toString())
 
-            val intent = Intent(this, NotificationService::class.java)
-            intent.putExtra(NotificationService.NOTIFICATION_NAME, name)
-
-            startService(intent)
+            LocalBroadcastManager.getInstance(this).sendBroadcast(intent)
         }
     }
 
-}
+    override fun onStart() {
+        super.onStart()
+        val filter = IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED)
+        bluetoothReceiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                Log.d(LOG_TAG, "Bluetooth state changed")
+            }
 
+        }
+        registerReceiver(bluetoothReceiver, filter)
+
+        val localFilter = IntentFilter(GREET_ACTION)
+        greetingReceiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                val name = intent?.getStringExtra(EXTRA_NAME)
+
+                Log.d(LOG_TAG, name)
+            }
+
+        }
+        LocalBroadcastManager.getInstance(this).registerReceiver(greetingReceiver, localFilter)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(bluetoothReceiver)
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(greetingReceiver)
+    }
+
+    companion object {
+        const val LOG_TAG = "Broadcasts"
+        const val EXTRA_NAME = "com.lidaamber.myapplication.EXTRA_NAME"
+        const val GREET_ACTION = "com.lidaamber.myapplication.GREET"
+    }
+
+}
